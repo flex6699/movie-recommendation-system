ce19223d6d55e950f0f60057d364eb2521b3a37f
flex6699 <54765869+flex6699@users.noreply.github.com>
2024-03-28 00:38:12 +0530

    project app

---
+++ main.py
@@ -0,0 +1,55 @@
+import pandas as pd
+
+import streamlit as st
+import pickle
+import requests
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+def fetch_poster(id):
+    response=requests.get("https://api.themoviedb.org/3/movie/{}?api_key=187ddd0030ceb631624254ff8728e286&language=en-US".format(id))
+    data=response.json()
+    print(data["poster_path"])
+    return "https://image.tmdb.org/t/p/w500/"+data["poster_path"]
+def recommend(title):
+    idx=movies2idx[title];
+    if type(idx)==pd.Series:
+        idx=idx.iloc[0]
+    query=similarity[idx];
+    scores=cosine_similarity(query,similarity)
+    scores=scores.flatten()
+    recommended_idx=(-scores).argsort()[1:6]
+    recommended_movies=[]
+    recommended_posters=[]
+
+    for i in recommended_idx:
+        recommended_posters.append(fetch_poster(movies_list["id"].iloc[i]));
+        recommended_movies.append(movies_list["original_title"].iloc[i]);
+    return recommended_movies,recommended_posters
+
+st.title("Movie Recommender System")
+movies_list=pickle.load(open("movies.pkl","rb"))
+similarity=pickle.load(open("similarity.pkl","rb"))
+movies_list=pd.DataFrame(movies_list)
+movies2idx=pd.Series(movies_list.index,index=movies_list["original_title"])
+
+
+select_movie_name=st.selectbox("Type Movie Name",movies_list["original_title"].values)
+if st.button("Recommend"):
+    names,posters=recommend(select_movie_name)
+    col1,col2,col3,col4,col5=st.columns(5)
+    with col1:
+        st.text(names[0])
+        st.image(posters[0])
+    with col2:
+        st.text(names[1])
+        st.image(posters[1])
+    with col3:
+        st.text(names[2])
+        st.image(posters[2])
+    with col4:
+        st.text(names[3])
+        st.image(posters[3])
+    with col5:
+        st.text(names[4])
+        st.image(posters[4])
